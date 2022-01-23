26b3d1c88cb2b41f35405d3b500d550d3e1c54f8
storyknight <realstoryknight@gmail.com>
2022-01-22 21:21:18 -0600

    add layout xml and basic buttons

--- app/src/main/java/com/dong/project/MainActivity.java
+++ app/src/main/java/com/dong/project/MainActivity.java
@@ -2,13 +2,50 @@ package com.dong.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private final int PREFERENCE_FILE_KEY = 69;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("ResourceType")
+    public void login(View view) {
+        EditText amountView = (EditText) findViewById(R.id.editTextNumberPassword);
+        if (amountView == null) {
+            return;
+        }
+
+        else if (amountView.getText().equals(0)) {
+            return;
+        }
+
+        Context context = getActivity();
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        // check pass
+        int pass = getResources().getInteger(R.integer.pass);
+        System.out.println("current pass:" + pass);
+        if (pass == 0) {
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putInt(getString(R.integer.pass), Integer.parseInt(String.valueOf(amountView.getText())));
+            editor.apply();
+            System.out.println("pass created");
+        }
+
+
+    }
+
+    private Context getActivity() {
+        return MainActivity.this;
     }
 }
